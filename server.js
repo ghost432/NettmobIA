ec0a1aa9de1f658fdffb69ccbed72ee9aa3d9ae5
TheoKamdem <youngtheophile@gmail.com>
2025-09-29 15:35:33 +0100

    change sever.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // NettmobIA/server.js
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+if (!process.env.PORT) process.env.PORT = process.env.APP_PORT || '4001'
 const { start } = require('./packages/server/dist/index')
-start().catch((e) => {
-  console.error(e)
-  process.exit(1)
-})
+start().catch((e)=>{ console.error(e); process.exit(1) })
